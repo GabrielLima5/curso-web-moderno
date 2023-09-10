11952ad847fa6d7b4c8e6476a3603d32e6fa24a4
Gabriel Lima <121446890+GabrielLima5@users.noreply.github.com>
2023-09-09 21:36:37 -0300

    wrong path

--- exercicios/src/componentes/BomDia.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default (props) => 
-    <div>
-        <h1>Olá {props.nome}</h1>
-        <h2>Você tem {props.idade} anos.</h2>
-    </div>
--- exercicios/src/componentes/Teste.jsx
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-
-export default (props) => <h1>Olá {props.nome}! Você torce para o {props.time} e mora em {props.cidade}</h1>
